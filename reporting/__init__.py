c1076f9f7d0912b375cc04f622d8be448d7b3756
Misha Mesarcik <m.mesarcik@uva.nl>
2021-01-25 13:15:10 +0100

    Rebuttal barplotting, heatmap and statistics added

--- reporting/__init__.py
+++ reporting/__init__.py
@@ -7,7 +7,7 @@ from .plotter import plot_max_avg
 from .embedding import plot_overlay
 from .embedding import plot_knn_neighbours
 from .embedding import plot_radius_neighbours 
-from .heatmap import heatmap, get_max_parameters, sensitivity_heatmap
+from .heatmap import heatmap, get_max_parameters, sensitivity_heatmap,sensitivity_heatmap_shared
 from .barplotter import barplot, barplot_shared
 from .statistics import get_statistics
 
