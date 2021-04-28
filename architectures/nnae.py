e7b526462920f4ae5d6c2d901fd031bf9fbd4230
Misha Mesarcik <m.mesarcik@uva.nl>
2021-04-28 17:12:46 +0200

    nnae architecture updated

--- architectures/nnae.py
+++ architectures/nnae.py
@@ -6,6 +6,7 @@ from models import (Encoder,
                    Autoencoder)
 
 from sklearn import neighbors
+from inference import infer
 
 from utils.plotting  import  (generate_and_save_images,
                              generate_and_save_training)
@@ -15,19 +16,19 @@ from model_config import *
 from .helper import end_routine
 
 optimizer = tf.keras.optimizers.Adam()
-neighbours = 3
-
-def nn_loss(x,x_hat,z,_z,nbrs):
-
-    neighbours_dist, neighbours_idx =  nbrs.kneighbors(z_query,return_distance=True)#KNN
-    z_stacked = tf.stack([test_images]*neighbours,axis=1)
-    nle = tf.math.abs(tf.subtract(z_stacked,_z[neighbors]))
-
-    return tf.reduce_mean(nle)
+neighbours = 5
 
 
-@tf.function
-def train_step(model, x, _z, nbrs):
+def nn_loss(x,x_hat,z,nn):
+    x_stacked = tf.stack([x]*neighbours,axis=1)
+    nle = tf.reduce_mean(tf.math.abs(tf.subtract(x_stacked,nn)))
+    recon = tf.reduce_mean(tf.math.abs(tf.subtract(x,x_hat)))
+
+    return nle + recon
+
+
+#@tf.function
+def train_step(model, x, nn):
     """Executes one training step and returns the loss.
 
     This function computes the loss and gradients, and uses the latter to
@@ -36,21 +37,30 @@ def train_step(model, x, _z, nbrs):
     with tf.GradientTape() as tape:
         x_hat = model(x)
         z = model.encoder(x)
-        loss = nn_loss(x,x_hat,z, _z, nbrs)
+        loss = nn_loss(x,x_hat,z, nn)
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     return loss
 
-def train(ae,train_dataset,test_images,test_labels,args,verbose=True,save=True):
+def train(ae,train_dataset,train_images, test_images,test_labels,args,verbose=True,save=True):
     ae_loss= []
     for epoch in range(args.epochs):
         start = time.time()
+        _z = infer(ae.encoder, train_images, args, 'encoder')# ae.encoder(train_dataset)
+        _x = infer(ae, train_images, args, 'AE')# ae.encoder(train_dataset)
         for image_batch in train_dataset:
-            _z = ae.encoder(image_batch)
+
+            z = ae.encoder(image_batch)
+
             nbrs = neighbors.NearestNeighbors(n_neighbors= neighbours,
                                               algorithm='ball_tree',
                                               n_jobs=-1).fit(_z) 
-            auto_loss  =  train_step(ae,image_batch, _z, nbrs)
+
+            neighbours_idx =  nbrs.kneighbors(z.numpy(),return_distance=False)
+
+            nn = tf.convert_to_tensor(_x[neighbours_idx], dtype=tf.float32)
+
+            auto_loss  =  train_step(ae,image_batch, nn)
 
         generate_and_save_images(ae,
                                  epoch + 1,
@@ -72,7 +82,7 @@ def train(ae,train_dataset,test_images,test_labels,args,verbose=True,save=True):
 
 def main(train_dataset,train_images,train_labels,test_images,test_labels, test_masks,args):
     ae = Autoencoder(args)
-    ae = train(ae,train_dataset,test_images,test_labels,args)
+    ae = train(ae,train_dataset, train_images, test_images,test_labels,args)
     end_routine(train_images, test_images, test_labels, test_masks, [ae], 'NNAE', args)
 
     
