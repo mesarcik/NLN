7b0a13c0da0acaac00b8271d399ece39f8a0f2fe
mesarcik <m.mesarcik@uva.nl>
2021-05-05 15:41:01 +0200

    nnae updated to use x

--- architectures/nnae.py
+++ architectures/nnae.py
@@ -17,12 +17,15 @@ from .helper import end_routine
 
 ae_optimizer = tf.keras.optimizers.Adam()
 enc_optimizer = tf.keras.optimizers.Adam()
-neighbours = 5
+NNEIGHBOURS= 5
 
 
-def nn_loss(x,x_hat,dists):
-    nle = tf.reduce_mean(dists)
-    recon = 0*tf.reduce_mean(tf.math.abs(tf.subtract(x,x_hat))) # a hack to make tf work ?????
+def nn_loss(x,x_hat,neigh):
+    
+    x_stack = tf.stack([x]*NNEIGHBOURS,axis=1)
+    
+    nle = tf.reduce_mean(tf.math.abs(tf.subtract(x_stack,neigh))) 
+    recon = tf.reduce_mean(tf.math.abs(tf.subtract(x,x_hat))) 
 
     return nle + recon
 
@@ -34,39 +37,39 @@ def z_loss(dists):
     return tf.reduce_mean(dists,name='encloss')
 
 @tf.function
-def train_step(model, x, dists):
+def train_step(model, x, neighs):
     """Executes one training step and returns the loss.
 
     This function computes the loss and gradients, and uses the latter to
     update the model's parameters.
     """
-    with tf.GradientTape() as ae_tape, tf.GradientTape() as enc_tape:
+    with tf.GradientTape() as ae_tape:
         x_hat = model(x)
-        ae_loss = l2_loss(x,x_hat)
-        enc_loss = nn_loss(x,x_hat,dists)
+        ae_loss = nn_loss(x,x_hat,neighs)
+
     ae_gradients = ae_tape.gradient(ae_loss, model.trainable_variables)
-    enc_gradients = enc_tape.gradient(enc_loss, model.encoder.trainable_variables)
 
     ae_optimizer.apply_gradients(zip(ae_gradients, model.trainable_variables))
-    enc_optimizer.apply_gradients(zip(enc_gradients, model.encoder.trainable_variables))
-    return ae_loss, enc_loss
+    return ae_loss
 
 def train(ae,train_dataset,train_images, test_images,test_labels,args,verbose=True,save=True):
-    ae_loss, enc_loss = [], []
+    ae_loss = []
     train_dataset = tf.data.Dataset.from_tensor_slices(train_images).batch(BATCH_SIZE)
     for epoch in range(args.epochs):
         start = time.time()
-        _z = infer(ae.encoder, train_images, args, 'encoder')# ae.encoder(train_dataset)
-        nbrs = neighbors.NearestNeighbors(n_neighbors= neighbours, algorithm='ball_tree', n_jobs=-1).fit(_z) 
-        z = ae.encoder(test_images)
 
-        neighbours_dist, _ =  nbrs.kneighbors(z.numpy(), return_distance=True)
-        dists = tf.convert_to_tensor(neighbours_dist, dtype=tf.float32)
+        _z = infer(ae.encoder, train_images, args, 'encoder')
+        _x = infer(ae , train_images, args, 'AE')
+        nbrs = neighbors.NearestNeighbors(n_neighbors= NNEIGHBOURS, algorithm='ball_tree', n_jobs=-1).fit(_z) 
 
-        dists = tf.data.Dataset.from_tensor_slices(dists).batch(BATCH_SIZE)
+        neighbours_dist, neighbours_idx  =  nbrs.kneighbors(_z, return_distance=True)
+        neighbours = _x[neighbours_idx]
 
-        for image_batch,dist in zip(train_dataset,dists):
-            auto_loss, encoder_loss =  train_step(ae,image_batch, dist)
+        neighbours = tf.convert_to_tensor(neighbours, dtype=tf.float32)
+        neighbours_dataet = tf.data.Dataset.from_tensor_slices(neighbours).batch(BATCH_SIZE)
+
+        for image_batch, neighs in zip(train_dataset,neighbours_dataet):
+            auto_loss =  train_step(ae,image_batch, neighs)
 
         generate_and_save_images(ae,
                                  epoch + 1,
@@ -76,13 +79,11 @@ def train(ae,train_dataset,train_images, test_images,test_labels,args,verbose=Tr
         save_checkpoint(ae,epoch, args,'NNAE','ae')
 
         ae_loss.append(auto_loss)
-        enc_loss.append(encoder_loss)
 
-        print_epoch('NNAE',epoch,time.time()-start,{'AE Loss':auto_loss.numpy(),
-                                                    'Encoder Loss':encoder_loss.numpy()},None)
+        print_epoch('NNAE',epoch,time.time()-start,{'AE Loss':auto_loss.numpy()},None)
 
-    generate_and_save_training([ae_loss,enc_loss],
-                                ['ae loss','encoder loss'],
+    generate_and_save_training([ae_loss],
+                                ['ae loss'],
                                 'NNAE',args)
     generate_and_save_images(ae,epoch,image_batch[:25,...],'NNAE',args)
 
