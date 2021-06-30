af1c4fbf7a336b5db528384ab596ca4c819fba02
mesarcik <m.mesarcik@uva.nl>
2021-06-30 12:28:51 +0200

    fixed reporting bugs

--- utils/metrics/__init__.py
+++ utils/metrics/__init__.py
@@ -4,4 +4,4 @@ sys.path.insert(1,'../..')
 
 from .calculate_metrics import calculate_metrics, get_classifcation, save_metrics
 from .nln_metrics import get_nln_metrics, nln, get_nln_errors
-from .mvtec_metrics import  accuracy_metrics
+from .mvtec_metrics import  accuracy_metrics, get_dists, iou_score
